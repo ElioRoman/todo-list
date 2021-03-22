2eacfb7bac0fab04f38ab8179b5340fb70eb4c1b
ElioRoman <kolomiets.roman30@gmail.com>
2021-03-22 19:10:11 +0200

    add file

---
+++ src/tasks/tasksGateway.jsx
@@ -0,0 +1,54 @@
+const baseUrl = 'https://5fedddb29708250017ce3f18.mockapi.io/api/v1/tasks';
+
+export const createTask = taskData => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskData),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to create new task');
+    }
+  });
+};
+
+export const fetchTasksList = () => {
+  return fetch(baseUrl)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(tasksList => {
+      return tasksList.map(({ _id, ...task }) => ({
+        id: _id,
+        ...task,
+      }));
+    });
+};
+
+export const updateTask = (taskId, taskData) => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskData),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to create new task');
+    }
+  });
+};
+
+export const deleteTask = taskId => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'DELETE',
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to delete task');
+    }
+  });
+};
